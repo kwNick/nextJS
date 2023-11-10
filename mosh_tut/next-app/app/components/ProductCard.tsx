5ae456f1ed7fab647020c45d790295f0d85107f6
Nickolas Piraino <kwnp17c@gmail.com>
2023-11-10 00:33:48 -0500

    Tailwind CSS on ProductCard; removing the module styles

--- mosh_tut/next-app/app/components/ProductCard.tsx
+++ mosh_tut/next-app/app/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import AddToCart from './AddToCart';
 import styles from './ProductCard.module.css';
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-800'>
         <AddToCart/>
     </div>
   )
