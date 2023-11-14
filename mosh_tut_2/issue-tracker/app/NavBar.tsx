a8b6799773637f3ff5fbba9e20fcdad3354a0271
Nickolas Piraino <kwnp17c@gmail.com>
2023-11-14 15:38:07 -0500

    styling the active navbar using ternary and classnames package

--- mosh_tut_2/issue-tracker/app/NavBar.tsx
+++ mosh_tut_2/issue-tracker/app/NavBar.tsx
@@ -1,7 +1,12 @@
+'use client';
 import Link from 'next/link'
 import React from 'react'
+import {usePathname} from 'next/navigation';
 import { FaBug } from "react-icons/fa";
+import classNames from 'classnames';
 const NavBar = () => {
+    const currentPath = usePathname();
+    console.log(currentPath);
     const links = [
         {label: 'Dashboard', href:'/'},
         {label: 'Issues', href:'/issues'},
@@ -10,9 +15,19 @@ const NavBar = () => {
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
         <Link href="/"><FaBug /></Link>
         <ul className="flex space-x-6">
+            {/* {links.map(link => <Link 
+            key={link.href}
+            className={`${link.href === currentPath ? 'text-zinc-900': 'text-zinc-500'} hover:text-zinc-800 transition-colors`}
+            href={link.href}>{link.label}</Link>
+            )} */}
             {links.map(link => <Link 
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-900 transition-colors"
+            className={classNames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+            })
+            }
             href={link.href}>{link.label}</Link>
             )}
         </ul>
