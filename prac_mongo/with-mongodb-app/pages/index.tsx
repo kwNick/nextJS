333df1097c977221615468cf54a9c4a775295d57
Nickolas Piraino <kwnp17c@gmail.com>
2023-12-01 14:31:42 -0500

    practicing nextjs-mongodb architecture

--- prac_mongo/with-mongodb-app/pages/index.tsx
+++ prac_mongo/with-mongodb-app/pages/index.tsx
@@ -14,12 +14,12 @@ export const getServerSideProps: GetServerSideProps<
     // `await clientPromise` will use the default database passed in the MONGODB_URI
     // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
     //
-    const client = await clientPromise
-    const db = client.db("myDatabase")
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
     //
     // Then you can execute queries against your database like so:
-    // db.find({}) //or any of the MongoDB Node Driver commands
-    console.log(clientPromise)
+    // db.find({}) or any of the MongoDB Node Driver commands
+
     return {
       props: { isConnected: true },
     }
@@ -253,6 +253,5 @@ export default function Home({
         }
       `}</style>
     </div>
-    
   )
 }
