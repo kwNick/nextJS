fbbe5117ffa52b3f124418609795a4083e95e19c
Nickolas Piraino <kwnp17c@gmail.com>
2024-01-10 15:38:38 -0500

    changed directory name of lee_rob_intro -> lee_rob_scratch

--- lee_rob_intro/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UbaFeUUrBg9zjaayTRZQhYJefWLYyY6eVAEiDoNbmAE=\"\n}"
--- lee_rob_intro/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"__rewrites":{"afterFiles":[],"beforeFiles":[],"fallback":[]},"/":"static/chunks/pages/index.js","/_not-found":"static/chunks/pages/_not-found.js","/about":"static/chunks/pages/about.js","sortedPages":["/","/_not-found","/about"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
