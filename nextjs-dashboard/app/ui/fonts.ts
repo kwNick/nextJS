ca7e59cf132400c4aadc64c8b5ad488df83b7242
Nickolas Piraino <kwnp17c@gmail.com>
2024-01-16 17:16:41 -0500

    layouts and pages chapter 4

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import {Lusitana, Inter} from 'next/font/google';
 // import {} from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'],display: 'swap'});
-// export const lusitana = Lusitana({ subsets: ['latin'], display: 'swap'})
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'], display: 'swap'})
