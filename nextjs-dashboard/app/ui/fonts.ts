e468ec1264b528cec75cd19af0ead216c3a84183
Nickolas Piraino <kwnp17c@gmail.com>
2024-01-16 16:33:17 -0500

    learn nextjs book  nextjs.org

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Lusitana, Inter} from 'next/font/google';
+// import {} from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'],display: 'swap'});
+// export const lusitana = Lusitana({ subsets: ['latin'], display: 'swap'})
